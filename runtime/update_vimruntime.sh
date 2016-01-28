f7ebdf2d5cb061006b576b0e289dc93f4141db7a
Wu Yongwei <wuyongwei@gmail.com>
2016-01-28 15:24:11 +0800

    Allow script to run outside the runtime directory.

--- runtime/update_vimruntime.sh
+++ runtime/update_vimruntime.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 VIMRUNTIME_RT="/cygdrive/c/Program Files (x86)/Vim/vim74"
+cd "`dirname \"$0\"`"
 rsync -avu --delete "autoload/" "$VIMRUNTIME_RT/autoload/"
 rsync -avu --delete "colors/"   "$VIMRUNTIME_RT/colors/"
 rsync -avu --delete "compiler/" "$VIMRUNTIME_RT/compiler/"
