55686399f10233830c4c71a39083a9fe046e31e3
Yongwei Wu <wuyongwei@gmail.com>
2017-01-18 08:00:28 +0800

    Remove the unnecessary upstream setting.

--- prepare_sync.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/sh
-git remote add upstream https://github.com/vim/vim.git
--- sync_vim.sh
+++ sync_vim.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
-git fetch upstream
-git merge -m 'Sync with upstream master.' upstream/master
+git fetch vim
+git merge -m 'Sync with upstream master.' vim/master
