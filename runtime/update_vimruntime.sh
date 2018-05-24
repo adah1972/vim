8adee34ac39b21553f6619c4e8362a776b0483a7
Wu Yongwei <wuyongwei@gmail.com>
2018-05-24 14:27:34 +0800

    Update Vim runtime path to 8.1.

--- runtime/update_vimruntime.sh
+++ runtime/update_vimruntime.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-VIMRUNTIME_RT="/cygdrive/c/Program Files (x86)/Vim/vim80"
+VIMRUNTIME_RT="/cygdrive/c/Program Files (x86)/Vim/vim81"
 cd "`dirname \"$0\"`"
 rsync -avu --delete "autoload/" "$VIMRUNTIME_RT/autoload/"
 rsync -avu --delete "colors/"   "$VIMRUNTIME_RT/colors/"
